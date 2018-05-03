353304ec9ec9fcc2ea06bb78bee53b512e2c71d5
ggengras <graemegengras@gmail.com>
2018-05-03 12:44:42 -0400

    Added calloc pointer NULL checking to strCat

--- common/glib.c
+++ common/glib.c
@@ -8,9 +8,13 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 char *strCat(char *str1, char *str2) {
     char *newString = calloc(strlen(str1) + strlen(str2) + 1, sizeof(char));
+
+    // Make sure calloc worked
+    assert(newString != NULL);
 
     // Populate newString
     strcpy(newString, str1);
