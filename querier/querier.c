f9d66058555ad05014c22c30710925c8728390aa
ggengras <graemegengras@gmail.com>
2018-05-13 21:12:23 -0400

    finalizing

--- querier/querier.c
+++ querier/querier.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[]) {
     char *input;
     while (1) {
         printf("[Query]: ");
-        if ((int)(input = readlinep(stdin)) == EOF) { // MALLOCS SPACE!
+        if ((input = readlinep(stdin)) == EOF) { // MALLOCS SPACE!
             // exit if EOF
             free(input);
             break;
